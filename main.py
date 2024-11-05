5f2d394a09ea4407320c144fa216acebd37d69b3
PoonamKalita18 <arunakalita2015@gmail.com>
2024-11-05 21:28:00 +0530

    holaa

---
+++ main.py
@@ -0,0 +1,20 @@
+T= ()
+print(T)
+
+T = (1,2,3)
+print(T)
+
+T = (1,"hello",98.9)
+print(T)
+
+T= ("bangtan", [3,2,1],(6,54,4) )
+print(T)
+
+T = ('p', 'o','o','n','a','m')
+print(T[0])
+print(T[3])
+
+NT = ("bangtan", [3,2,1],(6,54,4) )
+print(NT[0][3])
+print(NT[1][2])
+print(NT[2][0])
